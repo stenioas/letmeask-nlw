29b991d8fec8a334c924109a1432849e58f71564
stenioas <stenioas@gmail.com>
2021-06-22 21:36:24 -0300

    refactor(context): auth context migrated to component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
-import { useContext } from "react";
 import { useHistory } from "react-router-dom";
 
-import { AuthContext } from "../App";
+import { useAuth } from "../hooks/useAuth";
 import { Button } from "../component/Button";
 
 import illustrationImg from "../assets/images/illustration.svg";
@@ -12,7 +11,7 @@ import "../styles/auth.scss";
 
 export function Home() {
 	const history = useHistory();
-	const { user, signInWithGoogle } = useContext(AuthContext);
+	const { user, signInWithGoogle } = useAuth();
 
 	async function handleCreateRoom() {
 		if (!user) {
